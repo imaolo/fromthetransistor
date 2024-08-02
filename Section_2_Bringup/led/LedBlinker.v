239d3b61249fea8f4780168a2a6b49a5f80b99d4
mad360 <matt@dldipaolo.com>
2024-08-02 14:38:49 -0700

    improve comments and cleanup

--- Section_2_Bringup/led/LedBlinker.v
+++ Section_2_Bringup/led/LedBlinker.v
@@ -1,28 +1,51 @@
-module LedBlinker #(parameter PERIOD=1)(
-    input wire clk,
-    input wire reset,
-    output reg led
+module LedBlinker #(
+    parameter DEFAULT_PERIOD = 1,
+    parameter MIN_PERIOD = 1,
+    parameter MAX_PERIOD = 1000
+)(
+    input       clk,
+    input       reset,
+    input[31:0] period,
+    output reg  led
 );
+    /* verify parameters */
+    initial begin
+        if (MIN_PERIOD <= 0)
+            $fatal(1, "Mininum period must be greater than 0");
+        if (MAX_PERIOD > (2**31 - 1))
+            $fatal(1, "Maximum period must fit in integer");
+        if (MAX_PERIOD < MIN_PERIOD)
+            $fatal(1, "Maximum period must be greater than mininum period");
+        if (DEFAULT_PERIOD > MAX_PERIOD || DEFAULT_PERIOD < MIN_PERIOD)
+            $fatal(1, "Default period must be within max and min period");
+    end
 
-generate
-    if (PERIOD < 0) initial $fatal(1,"invalid period %d", PERIOD);
-endgenerate
+    /* define internal registers */
+    integer period_reg;
+    integer cnt;
 
-integer cnt = 0;
+    /* initialize registers */
+    initial begin
+        led = 0;
+        cnt = 0;
+        period_reg = (period == 0) ? DEFAULT_PERIOD : period;
+    end
 
-initial begin
-    led = 0;
-    cnt = 0;
-end
+    /* main toggle logic */
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            led <= 0;
+            cnt <= 0;
 
-always @(posedge clk) begin
-    if (reset) begin
-        led <= 0;
-        cnt <= 0;
-    end else if (cnt == PERIOD) begin
-        led <= ~led;
-        cnt <= 0;
-    end else cnt <= cnt + 1;
-end
+            if ((period > MAX_PERIOD || period < MIN_PERIOD) && period != 0)
+                $fatal(1, "period must be within max and min");
+
+            period_reg = (period == 0) ? DEFAULT_PERIOD : period;
+        end else if (cnt >= period_reg-1) begin
+            led <= ~led;
+            cnt <= 0;
+        end else
+            cnt++;
+    end
 
 endmodule
--- Section_2_Bringup/led/LedBlinker_TB.v
+++ Section_2_Bringup/led/LedBlinker_TB.v
@@ -1,31 +1,56 @@
 // iverilog -o LedBlinker_TB.vvp LedBlinker.v LedBlinker_TB.v
 // vvp LedBlinker_TB.vvp 
 
-`timescale 1ns/1ns
+module LEDBlinker_tb();
+    /* Test parameters */
+    parameter TEST_CYCLES = 500;
+    parameter MIN_PERIOD = 1;
+    parameter MAX_PERIOD = 141;
 
-module LedBlinkerTB;
+    /* test vars */
+    integer num_toggles = 0;
+    integer expected_toggles = 0;
+    integer i;
 
-parameter TEST_LEN = 10000;
-parameter PERIOD = 100;
-parameter EXPECTED_TOGGLES = TEST_LEN/20/PERIOD;
+    /* instantiate the blinker */
+    reg     reset = 0;
+    reg     clk = 0;
+    integer period;
+    wire    led;
+    LedBlinker u0 (
+        .clk(clk),
+        .reset(reset),
+        .period(period),
+        .led(led)
+    );
 
-reg reset = 0;
-reg clk = 0;
-integer num_toggles = 0;
-wire led;
+    /* start the clock */
+    initial forever #1 clk = ~clk;
 
-LedBlinker #(.PERIOD(PERIOD)) u0 (
-    .clk(clk),
-    .reset(reset),
-    .led(led));
+    /* track how many times the led changes */
+    always @(led) if (!reset) num_toggles++;
 
-always #10 clk = ~clk;
-always @(led) num_toggles++;
+    initial begin
+        for (i = MIN_PERIOD; i <= MAX_PERIOD; i++) begin
+            /* configure blinker */
+            period = i;
+            reset = 1;
 
-initial begin
-    #TEST_LEN; if (num_toggles != EXPECTED_TOGGLES) $fatal(1,"FAILED num : %d, exp : %d ", num_toggles, EXPECTED_TOGGLES);
-    $display("PASSED");
-    $finish;
-end
+            /* reset test counter */
+            num_toggles = 0;
 
-endmodule
+            /* The led should change after every period (/period) rising edges (/2) */
+            expected_toggles = TEST_CYCLES/period/2;
+
+            /* let the led blink */
+            #2 reset = 0;
+            #(TEST_CYCLES)
+
+            /* verify the number of blinks */
+            if (num_toggles != expected_toggles)
+                $fatal(1,"FAILED num : %d, exp : %d", num_toggles, expected_toggles);
+        end
+        $finish(0);
+    end
+
+endmodule;
