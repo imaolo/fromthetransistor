5ccca880244ddd0ddfd0e4c036b41c562e799394
mad360 <matt@dldipaolo.com>
2024-08-02 18:03:27 -0700

    transmitter moved to its own file

---
+++ Section_2_Bringup/uart1/transmitter.v
@@ -0,0 +1,54 @@
+module transmitter(
+    input[7:0]  data,      /* read buffer */
+    input       clk,       /* clk signal */
+    input       write_en,  /* start buffer transmission */
+    output reg  rdy,       /* ready for write */
+    output reg  tx         /* transmit wire */
+);
+    /* states */
+    parameter s_IDLE  = 2'b00;
+    parameter s_START = 2'b01;
+    parameter s_WRITE = 2'b10;
+    parameter s_STOP  = 2'b11;
+
+    /* initialize output signals */
+    initial begin
+        rdy = 1;
+        tx <= 1;
+    end
+
+    /* internal registers */
+    reg[7:0]  write_cnt = 0;
+    reg[1:0]  state     = s_IDLE;
+
+    always @(posedge clk) begin
+        case (state)
+            s_IDLE: begin
+                tx <= 1;
+                rdy <= 1;
+                if (write_en)
+                    state <= s_START;
+            end
+            s_START: begin
+                rdy <= 0;
+                tx <= 0;
+                write_cnt <= 0;
+                state <= s_WRITE;
+            end
+            s_WRITE: begin
+                if (write_cnt < 8) begin
+                    tx <= data[write_cnt];
+                    write_cnt++;
+                end
+                if (write_cnt >=8)
+                    state <= s_STOP;
+            end
+            s_STOP: begin
+                tx <= 1;
+                write_cnt <= 0;
+                state <= s_IDLE;
+            end    
+        endcase
+    end
+
+endmodule
---
+++ Section_2_Bringup/uart1/transmitter_tb.v
@@ -0,0 +1,76 @@
+`include "transmitter.v"
+
+module transmitter_tb();
+
+    /* instantiate the transmitter */
+    reg[7:0] data_in  = 0;
+    reg      clk      = 0;
+    reg      write_en = 0;
+    wire     rdy;
+    wire     tx;
+    transmitter t (
+        .data(data_in),
+        .clk(clk),
+        .write_en(write_en),
+        .rdy(rdy),
+        .tx(tx)
+    );
+
+    /* test vars */
+    integer i = 0;
+    reg[7:0] data = 8'b01010101;
+
+    /* start the clock */
+    initial forever #1 clk = ~clk;
+
+    /* start test */
+    initial begin
+        if (!tx)
+            $fatal(1, "tx should be idling");
+        if (!rdy)
+            $fatal(1, "tx should be ready");
+
+        /* lets begin transmission */
+        data_in = data;
+        write_en = 1;
+    
+        /* go to start state */
+        #4
+        if (rdy)
+            $fatal(1, "tx should not be ready");
+        if (tx)
+            $fatal(1, "should be reading the start bit");
+        
+        /* transmit all the bits and test */
+        for (i = 0; i<8; i++) begin
+            #2
+            if (tx != data[i])
+                $fatal(1, "incorrect bit %d", i);
+        end
+
+        /* disable transmission */
+        write_en = 0;
+
+        /* check the stop bit */
+        #2
+        if (!tx)
+            $fatal(1, "stop bit should be high");
+
+        /* shouldnt be ready yet */
+        if (rdy)
+            $fatal(1, "shouldnt be ready yet");
+
+        /* back to idle */
+        #2
+        if (!rdy)
+            $fatal(1, "should be ready");
+
+        /* test idle */
+        for (i = 0; i<20; i++)
+            #1 if (!tx) $fatal(1, "improper idle");
+
+        $finish(0);
+    
+    end
+
+endmodule
