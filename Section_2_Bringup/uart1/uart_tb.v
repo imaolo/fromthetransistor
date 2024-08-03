51062582c145f0d5f9244fa052d2f6c2016ba165
mad360 <matt@dldipaolo.com>
2024-08-02 21:16:03 -0700

    test data init, tie tx and rx together correctly, make sure signals are correct, tests are passing

--- Section_2_Bringup/uart1/uart_tb.v
+++ Section_2_Bringup/uart1/uart_tb.v
@@ -3,14 +3,18 @@
 module uart_tb();
 
     /* define the clock */
-    reg  clk;
+    reg  clk = 0;
+
+    /* some test vars */
+    reg[7:0] data;
+    integer i;
 
     /* declare uart1 */
     reg[7:0]  din1 = 0;
     wire[7:0] dout1;
     reg       wr_en1 = 0;
     reg       rd_en1 = 0;
-    reg       rx1  = 0;
+    wire      rx1;
     wire      rd_rdy1;
     wire      wr_rdy1;
     wire      tx1;
@@ -31,51 +35,51 @@ module uart_tb();
     wire[7:0] dout2;
     reg       wr_en2 = 0;
     reg       rd_en2 = 0;
-    reg       rx2 = 0;
+    wire      rx2;
     wire      rd_rdy2;
     wire      wr_rdy2;
     wire      tx2;
     uart u2 (
         .din(din2),
         .clk(clk),
-        .wr_en(wr_en1),
-        .rd_en(rd_en1),
+        .wr_en(wr_en2),
+        .rd_en(rd_en2),
         .rx(rx2),
         .tx(tx2),
-        .rd_rdy(rd_rdy1),
-        .wr_rdy(wr_rdy1),
-        .dout(dout1)
+        .rd_rdy(rd_rdy2),
+        .wr_rdy(wr_rdy2),
+        .dout(dout2)
     );
+
+    assign rx1 = tx2;
+    assign rx2 = tx1;
 
     /* start the clock */
     initial forever #1 clk = ~clk;
 
-    initial begin
-        rx1 <= tx2;
-        rx2 <= tx1;
+initial begin
 
-        if (!tx1)
-            $fatal(1, "u1 should be idling");
-        if (!tx2)
-            $fatal(1, "u2 should be idling");
-        #2
+    /* both data outs should be low */
+    if (dout1 != 8'b0)
+        $fatal(1, "1. dout1 should be low - %b", dout1);
+    if (dout1 === 8'bz)
+        $fatal(1, "2. dout1 should be low - %b", dout1);
+    if (dout2 != 8'b0)
+        $fatal(1, "1. dout2 should be low - %b", dout2);
+    if (dout2 === 8'bz)
+        $fatal(1, "2. dout2 should be low - %b", dout2);
 
-        /* lets write some data and see what happens */
-        // $fatal(1, "u2 is not reading %d", dout2);
-        din1 = 125;
-        din2 = 47;
-        wr_en1 = 1;
-        wr_en2 = 1;
-        rd_en2 = 1;
-        rd_en1 = 1;
-        #35
-        // $fatal(1, "u2 is not reading %d", dout2);
-        // if (dout1 != 46 || 1)
-        //     $fatal(1, "u1 is not reading %d", dout2);
 
-        
+    /* lets transmit some data */
 
-        $finish(1);
-    end;
+    din1 = 8'b11101000;
+    wr_en1 = 1;
+    rd_en2 = 1;
+    while (rd_rdy2 == 0)
+        #2;
+    if (dout2 != din1)
+        $fatal(1, "failed - %b - %b", dout2, din1);
+    $finish(0);
+end;
 
 endmodule
